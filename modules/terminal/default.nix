f3587134605a0fb3ae4c2a3c3e63ea90a1e055f6
ddodogames <69091682+ddodogames@users.noreply.github.com>
2025-06-16 14:07:35 +0300

    i will figure out later

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -3,7 +3,6 @@
 {
   imports =
     [ # Import all modules related to terminal.
-    ./starship.nix
     ./git.nix
     ./nix-alien.nix
     ];
--- modules/terminal/starship.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  programs.starship = {
-  enable = true;
-  presets = [ "pure-preset" ];
-  };
-
-}
